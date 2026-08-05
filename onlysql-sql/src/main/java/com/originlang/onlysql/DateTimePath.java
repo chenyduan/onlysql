7a569a59e50a6d98d490b68c18068379ac8404f1
chenyduan <37836223+chenyduan@users.noreply.github.com>
2026-08-05 20:50:12 +0800

    1

--- onlysql-sql/src/main/java/com/originlang/onlysql/DateTimePath.java
+++ onlysql-sql/src/main/java/com/originlang/onlysql/DateTimePath.java
@@ -1,65 +1,21 @@
 package com.originlang.onlysql;
 
-import com.originlang.onlysql.sql.CompareCondition;
-import com.originlang.onlysql.sql.InCondition;
-import com.originlang.onlysql.sql.IsNullCondition;
-import com.originlang.onlysql.sql.SingleCondition;
-import com.originlang.onlysql.sql.WhereCriteria;
+public class DatetimePath<T>
+{
 
-import java.util.Collection;
+    private String name;
+    private Class<T> type;
 
-public class DateTimePath<T> implements SqlPath {
-
-    private final String columnName;
-    private final Class<T> type;
-
-    public DateTimePath(String columnName, Class<T> type) {
-        this.columnName = columnName;
+    public DatetimePath(String name, Class<T> type) {
+        this.name = name;
         this.type = type;
     }
 
-    @Override
-    public String getColumnName() {
-        return columnName;
+    public String getName() {
+        return name;
     }
-
     public Class<T> getType() {
         return type;
     }
 
-    public WhereCriteria eq(Object value) {
-        return new SingleCondition(columnName, value);
-    }
-
-    public WhereCriteria gt(Object value) {
-        return new CompareCondition(columnName, ">", value);
-    }
-
-    public WhereCriteria gte(Object value) {
-        return new CompareCondition(columnName, ">=", value);
-    }
-
-    public WhereCriteria lt(Object value) {
-        return new CompareCondition(columnName, "<", value);
-    }
-
-    public WhereCriteria lte(Object value) {
-        return new CompareCondition(columnName, "<=", value);
-    }
-
-    public WhereCriteria ne(Object value) {
-        return new CompareCondition(columnName, "!=", value);
-    }
-
-    public WhereCriteria in(Collection<?> values) {
-        return new InCondition(columnName, values);
-    }
-
-    public WhereCriteria in(Object... values) {
-        return new InCondition(columnName, values == null ? java.util.Collections.emptyList() : java.util.Arrays.asList(values));
-    }
-
-    public WhereCriteria isNull() {
-        return new IsNullCondition(columnName);
-    }
 }
